fd09bee828717a6761aa33c3d1b8fb78c3dc4d26
Luke Scalf <homebrewerluke@gmail.com>
2017-04-28 08:47:26 -0700

    Adding booleans, expression parsing to let and return statements.

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -36,6 +36,44 @@ Program parseProgramChecked(String input) {
 
 }
 
+Statement parseSingleStatement(String input) {
+
+    Program program = parseProgramChecked(input);
+    expectNumStatements(program, 1);
+
+    return program.statements.first;
+
+}
+
+void testBooleanLiteral(Expression expression, bool expected) {
+
+    expect(expression, new isInstanceOf<Boolean>());
+    Boolean boolean = expression;
+
+    expect(boolean.value, equals(expected));
+    expect(boolean.tokenLiteral(), equals(expected.toString()));
+
+}
+
+void testBooleanParsing(String input, bool expected) {
+
+    Statement statement = parseSingleStatement(input);
+    expect(statement, new isInstanceOf<ExpressionStatement>());
+
+    ExpressionStatement expressionStatement = statement;
+    testBooleanLiteral(expressionStatement.expression, expected);
+
+}
+
+void testIdentifier(Expression expression, String value) {
+
+    expect(expression, new isInstanceOf<Identifier>());
+    Identifier identifier = expression;
+    expect(identifier.value, equals(value));
+    expect(identifier.tokenLiteral(), equals(value));
+
+}
+
 void testIntegerLiteral(Expression expression, int integerValue) {
 
     expect(expression, new isInstanceOf<IntegerLiteral>());
@@ -46,7 +84,7 @@ void testIntegerLiteral(Expression expression, int integerValue) {
 
 }
 
-void testInfix(String input, int leftValue, String operator, int rightValue) {
+void testInfix(String input, Object leftValue, String operator, Object rightValue) {
 
     Program program = parseProgramChecked(input);
 
@@ -58,9 +96,23 @@ void testInfix(String input, int leftValue, String operator, int rightValue) {
     expect(statement.expression, new isInstanceOf<InfixExpression>());
     InfixExpression expression = statement.expression;
 
-    testIntegerLiteral(expression.left, leftValue);
+    testLiteralExpression(expression.left, leftValue);
     expect(expression.operator, equals(operator));
-    testIntegerLiteral(expression.right, rightValue);
+    testLiteralExpression(expression.right, rightValue);
+
+}
+
+void testLiteralExpression(Expression expression, Object expected) {
+
+    if (expected is int) {
+        testIntegerLiteral(expression, expected);
+    } else if (expected is String) {
+        testIdentifier(expression, expected);
+    } else if (expected is bool) {
+        testBooleanLiteral(expression, expected);
+    } else {
+        fail("type of expression not handled: ${expected.runtimeType}");
+    }
 
 }
 
@@ -72,7 +124,7 @@ void testPrecedence(String input, String expected) {
 
 }
 
-void testPrefix(String input, String operator, int integerValue) {
+void testPrefix(String input, String operator, Object expectedValue) {
 
     Program program = parseProgramChecked(input);
 
@@ -85,7 +137,7 @@ void testPrefix(String input, String operator, int integerValue) {
     PrefixExpression expression = statement.expression;
 
     expect(expression.operator, equals(operator));
-    testIntegerLiteral(expression.right, integerValue);
+    testLiteralExpression(expression.right, expectedValue);
 
 }
 
@@ -173,6 +225,10 @@ void main() {
 
         testPrefix("!5;", "!", 5);
         testPrefix("-15;", "-", 15);
+        testPrefix("!foobar;", "!", "foobar");
+        testPrefix("-foobar;", "-", "foobar");
+        testPrefix("!true;", "!", true);
+        testPrefix("!false;", "!", false);
 
     });
 
@@ -186,6 +242,17 @@ void main() {
         testInfix("5 < 5;", 5, "<", 5);
         testInfix("5 == 5;", 5, "==", 5);
         testInfix("5 != 5;", 5, "!=", 5);
+        testInfix("foobar + barfoo;", "foobar", "+", "barfoo");
+        testInfix("foobar - barfoo;", "foobar", "-", "barfoo");
+        testInfix("foobar * barfoo;", "foobar", "*", "barfoo");
+        testInfix("foobar / barfoo;", "foobar", "/", "barfoo");
+        testInfix("foobar > barfoo;", "foobar", ">", "barfoo");
+        testInfix("foobar < barfoo;", "foobar", "<", "barfoo");
+        testInfix("foobar == barfoo;", "foobar", "==", "barfoo");
+        testInfix("foobar != barfoo;", "foobar", "!=", "barfoo");
+        testInfix("true == true", true, "==", true);
+        testInfix("true != false", true, "!=", false);
+        testInfix("false == false", false, "==", false);
 
     });
 
@@ -202,6 +269,17 @@ void main() {
         testPrecedence("5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))");
         testPrecedence("5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))");
         testPrecedence("3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))");
+        testPrecedence("true", "true");
+        testPrecedence("false", "false");
+        testPrecedence("3 > 5 == false", "((3 > 5) == false)");
+        testPrecedence("3 < 5 == true", "((3 < 5) == true)");
+
+    });
+
+    test("test boolean expression", () {
+
+        testBooleanParsing("true;", true);
+        testBooleanParsing("false;", false);
 
     });
 
